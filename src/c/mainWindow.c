7d1aba9590f1b0f100c9aab82e76b4d6f22297ae
COZMIKDX <tacocatdx@gmail.com>
2023-06-11 18:08:27 -0500

    first commit. Got a test resource and font.

---
+++ src/c/mainWindow.c
@@ -0,0 +1,19 @@
+#include <pebble.h>
+#include "mainWindow.h"
+
+void init(void)
+{
+    main_window_create();
+}
+
+void deinit(void)
+{
+    main_window_destroy();
+}
+
+int main(void)
+{
+    init();
+    app_event_loop();
+    deinit();
+}
---
+++ src/c/mainWindow.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <pebble.h>
+
+void main_window_create();
+void main_window_destroy();
