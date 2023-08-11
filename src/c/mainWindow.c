c83e633585472d73a642fbb7d173a4a61cebd787
COZMIKDX <tacocatdx@gmail.com>
2023-08-10 20:19:11 -0500

    Removed canvas function. Replaced old time text setup with my new implementation.

--- src/c/mainWindow.c
+++ src/c/mainWindow.c
@@ -13,11 +13,8 @@ static Layer *canvas;
 
 struct Images * image_holder;
 struct Images * digit_image_holder;
+struct Texts * text_holder;
 
-
-static void canvas_update_proc(Layer *layer, GContext *ctx)
-{
-}
 
 // Get the time and update the time text layer.
 static void update_time() {
@@ -30,7 +27,7 @@ static void update_time() {
     strftime(buffer, sizeof(buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
 
     // Change the text layer for time to reflect the current time.
-    text_layer_set_text(time_layer, buffer);
+    update_text(text_holder->text_array[0], buffer)
 }
 
 // To be called when a tick happens. For this watchface, that'll be every minute.
@@ -47,25 +44,14 @@ static void window_load(Window *window)
     // Get the layer for this window to put stuff on it.
     Layer *window_layer = window_get_root_layer(s_window);
     GRect window_bounds = layer_get_bounds(window_layer);
+
+    // Setting images
     image_holder = init_images_struct(NUMBER_OF_IMAGES);
-
-    // Setting up the layer I'll draw on. Probably won't use it.
-    canvas = layer_create(window_bounds);
-    layer_set_update_proc(canvas, canvas_update_proc);
-    layer_add_child(window_layer, canvas);
-
-    // Setting up the background image
     add_image(image_holder, window_bounds, RESOURCE_ID_WEATHER_BG, window_layer);
 
-    // Time layer
-    time_font_16 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_TIME_24));
-    time_layer = text_layer_create(GRect(0, 0, 128, 100));
-    text_layer_set_background_color(time_layer, GColorBlack);
-    text_layer_set_text_color(time_layer, GColorWhite);
-    text_layer_set_font(time_layer, time_font_16);
-    text_layer_set_text_alignment(time_layer, GTextAlignmentLeft);
-    text_layer_set_text(time_layer, "00:00"); // temporary text until the tick handler runs
-    layer_add_child(window_layer, text_layer_get_layer(time_layer));
+    // Text
+    text_holder = init_texts_struct((uint32_t) 1, GColorWhite, GColorClear, RESOURCE_ID_TIME_24, window_layer);
+    add_text(text_holder, GRect(0,0,128,100), "00:00", window_layer);
 }
 
 void window_unload(Window *window)
